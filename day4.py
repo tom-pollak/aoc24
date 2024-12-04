d92c3c10df1759ec786f71d4649d2409180da5b8
Tom Pollak <tompollak1000@gmail.com>
2024-12-04 16:15:36 +0000

    cleanup

--- day4.py
+++ day4.py
@@ -1,12 +1,12 @@
-# %%
 # fmt: off
 import numpy as np
+import torch as t
+import torch.nn.functional as F
 from jaxtyping import Float, Int, jaxtyped
 from beartype import beartype as typechecker
 
+# ████████████████████████████████████  pt1  █████████████████████████████████████
 
-
-np.set_printoptions(linewidth=200)
 s2i = {s: i for i, s in enumerate("XMAS")}
 
 with open("assets/day4_b.txt", "r") as f:
@@ -14,8 +14,6 @@ with open("assets/day4_b.txt", "r") as f:
 
 q = np.arange(4)
 n, m = inp.shape
-
-# %%
 
 @jaxtyped(typechecker=typechecker)
 def count_seq(arr: Int[np.ndarray, "*n m"], pattern: Int[np.ndarray, "d"]) -> int:
@@ -25,7 +23,6 @@ def count_seq(arr: Int[np.ndarray, "*n m"], pattern: Int[np.ndarray, "d"]) -> in
     windows: Float[np.ndarray, "*n m-d+1 d"] = np.lib.stride_tricks.sliding_window_view(arr, len(pattern), axis=-1)
     return np.sum(np.all(windows == pattern, axis=-1)).item()
 
-# %%
 views = np.concatenate([
     inp, # right
     np.flip(inp, axis=1), # left
@@ -46,4 +43,33 @@ for row in diagonals:
     pt1_answ += count_seq(row, q)
 print(f"Part 1: {pt1_answ}")
 
-# %%
+# ████████████████████████████████████  pt2  █████████████████████████████████████
+
+s2oh = {s: np.eye(4)[i] for i, s in enumerate("XMAS")}
+
+with open("assets/day4_b.txt", "r") as f:
+    inp = t.tensor([[s2oh[c] for c in line] for line in f.read().splitlines()], dtype=t.float32)
+
+
+Z = t.zeros(4).tolist() # blank space
+_, M, A, S = tuple(s2oh.values())
+
+# our kernel!
+k = t.tensor([
+    [M, Z, S],
+    [Z, A, Z],
+    [M, Z, S],
+], dtype=t.float32)
+
+sum_to_find = k.sum()
+
+views = t.stack([
+    k,
+    k.flip(1),
+    t.rot90(k, 1, [0,1]),
+    t.rot90(k, -1, [0,1]),
+])
+
+output = F.conv2d(input=inp.permute(2, 0, 1)[None], weight=views.permute(0, 3, 1, 2))
+pt2_answ = t.sum(output == sum_to_find).item()
+print(f"Part 2: {pt2_answ}")
