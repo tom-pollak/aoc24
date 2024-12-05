c399f8d0deeecd5d0a2317ea2e9c7e5302db6e92
Tom Pollak <tompollak1000@gmail.com>
2024-12-05 12:14:39 +0000

    nicer formatting

--- day5.py
+++ day5.py
@@ -1,3 +1,5 @@
+# fmt: off
+# ruff: noqa
 from collections import defaultdict
 from functools import cmp_to_key
 
@@ -13,40 +15,28 @@ for line in raw_rules.splitlines():
     rules[int(before)].add(int(after))
 updates = [[int(x) for x in line.split(",")] for line in raw_updates.splitlines()]
 
-
 # ████████████████████████████████████  pt1  █████████████████████████████████████
-
 
 def check(update: list[int]) -> bool:
     for i, v in enumerate(update):
-        r = rules.get(v, set())
-        prev = set(update[:i])
-        if prev.intersection(r):
-            return False
+        prev, after_v = set(update[:i]), rules.get(v, set())
+        if prev.intersection(after_v): return False
     return True
 
-
-def get_mid(arr):
-    return arr[len(arr) // 2]
-
+def get_mid(arr): return arr[len(arr) // 2]
 
 pt1 = sum(get_mid(u) for u in updates if check(u))
 print(f"Part 1: {pt1}")
 
 # ████████████████████████████████████  pt2  █████████████████████████████████████
 
-
 @cmp_to_key
 def compare(x, y):
     x_r, y_r = rules.get(x, set()), rules.get(y, set())
-    if x in y_r and y in x_r:
-        raise ValueError("unsortable!")
-    if y in x_r:
-        return -1
-    elif x in y_r:
-        return 1
+    if x in y_r and y in x_r: raise ValueError("unsortable!")
+    if y in x_r: return -1
+    if x in y_r: return 1
     return 0
-
 
 pt2 = sum(get_mid(sorted(u, key=compare)) for u in updates if not check(u))
 print(f"Part 2: {pt2}")
